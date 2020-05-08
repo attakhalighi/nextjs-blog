3d390333e82b3dd1bdb227cef2cb07c13662f7c9
atakhalighi <ata.khalighi@gmail.com>
2020-05-08 17:36:24 +0430

    Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>One of the founders, analysts and developers of popular Iranian blogging service; Persianblog.ir (formerly Persianblog.com) which eventually turned into the biggest community for Iranian in the Internet. In 2008, the service scored more than 500,000 blogs and was visited 6 million times per month from all over the world.
-I spend most of my time on developing web based applications, managing teams and developing new start-ups. You can contact me on <a href="https://twitter.com/atakhalighi">Twitter</a>!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
-    </Layout >
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
